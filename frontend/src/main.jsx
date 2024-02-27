17442bc133fe72df1fb0a0b85b65e0a82d60af5b
James <james@jameskr.dev>
2024-02-26 20:49:56 -0500

    Remove uuid cookie

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Cookie from "js-cookie"
 
-console.log("starting app")
-const d = new Date
-
-//uuid cookie for persisting things, can be replaced by something more robust later
-Cookie.set("uuid",self.crypto.randomUUID())
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
