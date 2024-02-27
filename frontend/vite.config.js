8e4c97b20d9129f412a4bc047ea9cb0bf3aca7fb
James <james@jameskr.dev>
2024-02-26 20:45:37 -0500

    Add tailwindcss, prep for shadcn/ui
    - Prep includes adding components.json and jsconfig.json.
    - Reconfigure vite.config.js to include @ alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     watch: null,
     port: 5173,
     proxy: {
-      "/api": {
-        target: "http://localhost:5174",
-        changeOrigin: true,
-      }
+      "/api": { target: "http://localhost:5174", changeOrigin: true }
     }
-  },
-  plugins: [
-     react()]
-
+  }
 })
